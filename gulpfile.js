d7b538411bab060b6c7ac39d09667e45a8b94749
Maciej Walaszczyk <walaszczykm@gmail.com>
2017-10-17 18:06:50 +0200

    basic sass structure and home page

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,24 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const concat = require('gulp-concat')
 const cleanCSS = require('gulp-clean-css')
+const image = require('gulp-image')
 
 gulp.task('sass', () =>
-  gulp.src('./sass/*.scss')
+  gulp.src('./sass/**/*.scss')
   .pipe(sass())
-  .pipe(concat('style.min.css'))
+  .pipe(concat('style.css'))
   .pipe(cleanCSS())
   .pipe(gulp.dest('./css/'))
 )
 
-gulp.task('default', ['sass'])
+gulp.task('watch:sass', () =>
+  gulp.watch('./sass/**/*.scss', ['sass'])
+)
+
+gulp.task('image', () =>
+  gulp.src('./img/*')
+  .pipe(image())
+  .pipe(gulp.dest('./img/'))
+)
+
+gulp.task('default', ['sass', 'watch:sass'])
